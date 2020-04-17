f7654d21e292b186ec0cbbcf4a180e7306a5650c
Andy Chen <chenyangpoppin@hotmail.com>
2020-04-17 17:18:14 +0800

    Setup Vue single file component demo

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <h3><img class="head-logo" src="./assets/logo.png">{{ headline }}</h3>
+    <todo-list>
+      <todo-item v-for="item in list" :key="item.id" :id="item.id" :title="item.title" :del="item.del" @delete="deleteHandler">
+      </todo-item>
+    </todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    TodoItem
+  },
+  data: function() {
+    return {
+      headline: 'Learning Objects in The Year 2020',
+      list: [
+        { 
+          id: 0,
+          title: 'Learn Vue.js',
+          del: false
+        },
+        { 
+          id: 1, 
+          title: 'Learn IOS',
+          del: true
+        },
+        { 
+          id: 2,
+          title: 'Learn Java Spring MVC Framework',
+          del: false
+        },
+        {
+          id: 3,
+          title: 'Learn C++',
+          del: false
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteHandler: function(delId, delTitle) {
+      console.log('deleteHandler', delId, delTitle);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.head-logo {
+  width: 14px;
+  height: 14px;
+  margin-bottom: -1px;
+  margin-right: 10px;
+}
+</style>
---
+++ vue-demo/src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+<li :data-id="id">
+  <span v-if="!del" class="red">{{ title }}</span>
+  <span v-else class="line-through">{{ title }}</span>
+  <button v-show="!del" @click="clickHandler">Delete</button>
+</li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,  // The component will trigger error when id is not Number type
+    title: String,
+    del: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data: function() {
+    return {
+      value: Math.random()
+    };
+  },
+  methods: {
+    clickHandler: function() {
+      console.log('clickHandler');
+      this.$emit('delete', this.id, this.title);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.red {
+  color: #f00;
+}
+.line-through {
+  text-decoration: line-through;
+}
+</style>
