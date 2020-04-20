0d43907e16ef750dc55dad94eec18b601cc9db03
Andy Chen <chenyangpoppin@hotmail.com>
2020-04-20 21:48:56 +0800

    Add scaffold for timer practice

---
+++ vue-demo/src/views/clock.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ log('render') }}
+    {{ now }}
+    <button @click="start = !start">{{ start ? 'stop' : 'start' }}</button>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+export default {
+  data: function() {
+
+  }
+
+
+};
+</script>
