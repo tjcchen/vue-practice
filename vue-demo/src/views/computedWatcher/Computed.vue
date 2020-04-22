d8e539ea7d780289ff212b351ea92cb2347903aa
Andy Chen <chenyangpoppin@hotmail.com>
2020-04-22 22:10:18 +0800

    Adding code logic for computed properties and watcher

--- vue-demo/src/views/computedWatcher/Computed.vue
+++ vue-demo/src/views/computedWatcher/Computed.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
-    This is computed template
+  <div class="tab-body-computed-properties">
+    <p>reverseMessage1: {{ reverseMessage1 }}</p>
+    <p>reverseMessage2: {{ reverseMessage2() }}</p>
+    <p>{{ now }}</p>
+    <input v-model="message" />
   </div>
 </template>
 
 
 <script>
 export default {
-  name: 'computed'
+  name: 'computed',
+  data: function() {
+    return {
+      message: 'hello vue!'
+    };
+  },
+  computed: {
+    reverseMessage1: function() {
+      return this.message.split('')
+                         .reverse()
+                         .join('');
+    },
+    now: function() {
+      return Date.now();
+    }
+  },
+  methods: {
+    reverseMessage2: function() {
+      return this.message.split('')
+                         .reverse()
+                         .join('');
+    }
+  }
 }
 </script>
--- vue-demo/src/views/computedWatcher/Watcher.vue
+++ vue-demo/src/views/computedWatcher/Watcher.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    This is Watcher template!
+  <div class="tab-body-watcher">
+    <p>{{ fullName }}</p>
+    <div>
+      firstName: <input v-model="firstName" />
+    </div>
+    <div>
+      lastName: <input v-model="lastName" />
+    </div>
   </div>
 </template>>
 
 <script>
 export default {
   name: 'watcher',
+  data: function() {
+    return {
+      firstName: 'Andy',
+      lastName: 'Chen',
+      fullName: 'Andy Chen'
+    }
+  },
+  // computed: {
+  //   fullName: function() {
+  //     return this.firstName + ' ' + this.lastName;
+  //   }
+  // },
+  watch: {
+    firstName: function(val) {
+      this.fullName = val + ' ' + this.lastName;
+    },
+    lastName: function(val) {
+      this.fullName = this.firstName + ' ' + val;
+    }
+  }
 }
 </script>>
