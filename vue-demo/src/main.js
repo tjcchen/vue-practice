7f9c8d761ceaee7052ecfb5ae2a8f649586c6296
Andy Chen <chenyangpoppin@hotmail.com>
2020-04-22 10:55:49 +0800

    Resolve project npm dependencies problem

--- vue-demo/src/main.js
+++ vue-demo/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-// import App from './App.vue'
-import Index from './views/Index.vue'
+import Vue from 'vue';
+import antd from "ant-design-vue";
+// import App from './App.vue;
+import Index from './views/Index.vue';
+import "ant-design-vue/dist/antd.css";
 
 Vue.config.productionTip = false
+
+Vue.use(antd);
 
 new Vue({
   // render: h => h(App),
--- vue-demo/src/views/index.vue
+++ vue-demo/src/views/index.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
     This is an entry point template. {{ name }}
+    a-tab-pane
     <clock></clock>
+
+    <a-tabs>
+      <a-tab-pane key="clock" tab="时钟">
+       Clock Timer
+      </a-tab-pane>
+      <a-tab-pane key="Functional" tab="函数式组件">
+        Clock Functional
+      </a-tab-pane>
+    </a-tabs>
+
   </div>
 </template>
 
@@ -18,3 +29,13 @@ export default {
   }
 }
 </script>
+
+<style lang="less">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
