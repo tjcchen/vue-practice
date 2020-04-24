45ee3ac548cd90d12bc40ad3a088531bfe8c85db
Andy Chen <chenyangpoppin@hotmail.com>
2020-04-24 22:02:35 +0800

    Add scaffolding for provide and inject example

---
+++ vue-demo/src/views/provideInject/ChildrenA.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Children A</div>
+</template>
+
+<script>
+export default {
+  name: 'childrenA',
+  data: function() {
+    return {
+      
+    };
+  }
+}
+</script>>
---
+++ vue-demo/src/views/provideInject/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    This is a entry point file.
+    <ChildrenA />
+  </div>
+</template>
+
+<script>
+import ChildrenA from './ChildrenA';
+export default {
+  name: 'Index',
+  components: {
+    ChildrenA
+  },
+  data: function() {
+    return {
+
+    };
+  }
+}
+</script>>
