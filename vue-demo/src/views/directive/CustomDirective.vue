ec5788281a68636f9590e21eeb110ab8b8e57e9e
Andy Chen <chenyangpoppin@hotmail.com>
2020-04-24 17:27:38 +0800

    Add code logic to directives and custom directives

--- vue-demo/src/views/directive/CustomDirective.vue
+++ vue-demo/src/views/directive/CustomDirective.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="tab-body-custom-directive">
-    Custom Directive
+    <button @click="show = !show">Destroy</button>
+    <button v-if="show" v-append-text="`hello ${number}`" @click="number++">Click Me {{ number }}</button>
   </div>
 </template>>
 
 <script>
 export default {
   name: 'CustomDirective',
+  directives: {
+    appendText: {
+      // Please note the following custom directives' hook functions
+      // Eg: bind, inserted, update, componentUpdated, unbind
+      bind: function() {
+        console.log('bind');
+      },
+      inserted: function(el, binding) {
+        el.appendChild(document.createTextNode(binding.value));
+        console.log('inserted', el, binding);
+      },
+      update: function() {  // triggered when number changed
+        console.log('update');
+      },
+      componentUpdated: function(el, binding) {
+        el.removeChild(el.childNodes[el.childNodes.length - 1]);
+        el.appendChild(document.createTextNode(binding.value));
+
+        console.log('componentUpdated');
+        console.log(el.childNodes);  // text DOM nodes
+      },
+      unbind: function() {  // triggered when button disappeared
+        console.log('unbind');
+      }
+    }
+  },
   data: function() {
     return {
-
+      show: true,
+      number: 1
     };
   }
 }
--- vue-demo/src/views/directive/Directive.vue
+++ vue-demo/src/views/directive/Directive.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="tab-body-directive">
-    Directive
+    <h2>v-text</h2>
+    <div v-text="'hello vue'">hello world</div>
+
+    <h2>v-html</h2>
+    <div v-html="'<span style=&quot;color:red;&quot;>hello vue</span>'">hello world</div>
+    
+    <h2>v-show</h2>
+    <div v-show="show">hello vue</div>
+    <button v-on:click="show = !show">change show</button>
+    
+    <h2>v-if v-else-if v-else</h2>
+    <div v-if="number === 1">hello vue {{ number }}</div>
+    <div v-else-if="number === 2">hello world {{ number }}</div>
+    <div v-else>hello Tsinghua {{ number }}</div>
+
+    <h2>v-for v-bind</h2>
+    <div v-for="num in [1, 2, 3]" v-bind:key="num">hello vue {{ num }}</div>
+
+    <h2>v-on</h2>
+    <button v-on:click="number++">number++</button>
+
+    <h2>v-model</h2>
+    {{ message }}
+    <input v-model="message" />
+
+    <h2>v-pre</h2>
+    <div v-pre>
+      {{ this part of code will be shown on browsers directly }}
+    </div>
+
+    <h2>v-once</h2>
+    <div v-once>{{ number }}</div>
   </div>
 </template>>
 
@@ -9,7 +40,9 @@ export default {
   name: 'Directive',
   data: function() {
     return {
-
+      show: true,
+      number: 1,
+      message: 'vue'
     };
   }
 }
