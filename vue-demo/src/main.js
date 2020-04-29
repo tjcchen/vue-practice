8dd8f62df90a3e242912928019ae6280dc6bafdc
Andy Chen <chenyangpoppin@hotmail.com>
2020-04-29 15:07:30 +0800

    Add entry point file for jsx vs template example

--- vue-demo/src/main.js
+++ vue-demo/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import antd from "ant-design-vue";
 // import App from './App.vue;
-import Index from './views/ref/Index.vue';
+import Index from './views/jsxTemplate/Index.vue';
 import "ant-design-vue/dist/antd.css";
 
 Vue.config.productionTip = false
---
+++ vue-demo/src/views/jsxTemplate/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    Index Template
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
