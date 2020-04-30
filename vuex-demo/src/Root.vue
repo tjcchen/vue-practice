c9e7a6d885e2eb33ef577e253814973bd5632399
Andy Chen <chenyangpoppin@hotmail.com>
2020-04-30 15:30:41 +0800

    Add self-defined vuex source code

---
+++ vuex-demo/src/Root.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="root">
+    This is a root template, {{ msg }} : {{ count }}
+    <button @click="$store.commit('increment')">count++</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'root',
+  data: function() {
+    return {
+      msg: 'vue'
+    };
+  },
+  computed: {
+    count: function() {
+      return this.$store.state.count;
+    }
+  }
+}
+</script>
--- vuex-demo/src/main.js
+++ vuex-demo/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-import App from './App.vue'
+// import Vuex from 'vuex'
+// import App from './App.vue'
+import Vuex from './min-vuex';
+import Root from './Root.vue'
 
 Vue.use(Vuex);
 Vue.config.productionTip = false
@@ -35,7 +37,10 @@ const store = new Vuex.Store({
   }
 });
 
+Vue.prototype.$store = store;  // mount self-defined min-store to Vue prototype
+
 new Vue({
-  store,  // Make store become global variables
-  render: h => h(App),
+  // store,  // Make store become global variables
+  // render: h => h(App),
+  render: h => h(Root)
 }).$mount('#app')
