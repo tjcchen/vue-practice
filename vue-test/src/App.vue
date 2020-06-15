dd280dcd2619f5261b1137c61cfe3f336e1e9e8e
Andy Chen <chenyangpoppin@hotmail.com>
2020-06-15 21:21:20 +0800

    Add test component to this project

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test msg="hello from other side"></Test>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Test
   }
 }
 </script>
---
+++ vue-test/src/components/Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="test">
+    <h2>test... {{msg}}</h2>
+    <p>{{ value }}</p>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: {
+    msg: {
+      type: String,
+      default: 'message'
+    }
+  },
+  data: function() {
+    return {
+      value: 'vuejs'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.test {
+  color: #f00;
+}
+</style>
+
