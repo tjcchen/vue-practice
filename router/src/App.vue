81895b90499054f36f0f0e249ca13c75a1ea251f
Andy Chen <chenyangpoppin@hotmail.com>
2020-06-21 21:42:58 +0800

    Remove Hello World component from vue router project

--- router/src/App.vue
+++ router/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>A new Vue Router Project</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
