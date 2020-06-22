ec937c55b500391305416caa931c5ff2c2671857
Andy Chen <chenyangpoppin@hotmail.com>
2020-06-22 16:05:14 +0800

    Initialize vue router website

---
+++ router-site/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img class="vue-logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: { }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.vue-logo {
+  width: 60px;
+  height: 60px;
+}
+</style>
