276d5ecbc87f766b8a01c6d982dc4631f8da54b8
Andy Chen <chenyangpoppin@hotmail.com>
2020-06-22 09:31:15 +0800

    Add scaffolding for vue router project

--- router/src/App.vue
+++ router/src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <p>A new Vue Router Project</p>
+    <img alt="Vue logo" class="vue-logo-img" src="./assets/logo.png">
+    <h2>Vue Router Demo</h2>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-  components: {
-  }
+  components: { }
 }
 </script>
 
@@ -22,5 +21,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.vue-logo-img {
+  width: 100px;
+  height: 100px;
 }
 </style>
---
+++ router/src/components/RouterDemo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="link-groups">
+    <router-link to="/foo">Go To Foo</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
