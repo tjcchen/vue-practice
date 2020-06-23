2c88d4090b1bed630da4984cd3c5e694d2f0177d
Andy Chen <chenyangpoppin@hotmail.com>
2020-06-23 16:20:06 +0800

    Add active switch class for header component

--- router-site/src/components/Header.vue
+++ router-site/src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="header" class="header">
-    <router-link class="item" to="/home">Home</router-link>
-    <router-link class="item" to="/blog">Blog</router-link>
-    <router-link class="item" to="/profile">Profile</router-link>
-    <router-link class="item" to="/about">About</router-link>
-    <router-link class="item" to="/miscellaneous">Miscellaneous</router-link>
+  <div id="HEADER" class="header">
+    <router-link class="item" to="/home" data-page="home">Home</router-link>
+    <router-link class="item" to="/blog" data-page="blog">Blog</router-link>
+    <router-link class="item" to="/profile" data-page="profile">Profile</router-link>
+    <router-link class="item" to="/about" data-page="about">About</router-link>
+    <router-link class="item" to="/miscellaneous" data-page="miscellaneous">Miscellaneous</router-link>
   </div>
 </template>
 
@@ -15,13 +15,13 @@ export default {
 </script>
 
 <style scoped>
-#header {
+#HEADER {
   width: 1000px;
   height: 48px;
   text-align: center;
   margin: 6px auto 0;
 }
-#header .item {
+#HEADER .item {
   display: inline-block;
   width: 199px;
   height: 48px;
@@ -30,5 +30,11 @@ export default {
   color: #000;
   text-decoration: none;
   border-right: 1px solid #ccc;
+  -webkit-transition: all linear 300ms;
+  transition: all linear 300ms;
+}
+#HEADER .item:hover,
+#HEADER .item.router-link-active {
+  background: #46bd87;
 }
 </style>
