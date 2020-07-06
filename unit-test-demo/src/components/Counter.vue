901501d18ca9abd4302d9b21edf127dacaf58598
Andy Chen <chenyangpoppin@hotmail.com>
2020-07-06 21:26:21 +0800

    Add Counter component to unit test project

---
+++ unit-test-demo/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>Count: {{ count }}</div>
+    <button @click="() => increseCount()">Count++</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increseCount: function() {
+      this.count++;
+    }
+  }
+}
+</script>
