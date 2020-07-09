c35b8bd03694d2181d407cb7ba25abaf618ed8b3
Andy Chen <chenyangpoppin@hotmail.com>
2020-07-09 16:39:54 +0800

    Configure ant-design-vue relevant options

--- ant-design-vue-pro/src/main.js
+++ ant-design-vue-pro/src/main.js
@@ -3,7 +3,23 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// Import ant-design-vue and its corresponding css, this works fine if you are using css
+// import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.css";
+
+// import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.less";
+
+// import Button from "ant-design-vue/lib/button";
+// import "ant-design-vue/lib/button/style";
+
+// After babel configuration
+import { Button } from "ant-design-vue";
+
 Vue.config.productionTip = false;
+
+// Vue.use(Antd);
+Vue.use(Button);
 
 new Vue({
   router,
---
+++ ant-design-vue-pro/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true  // https://github.com/ant-design/ant-motion/issues/44
+      }
+    }
+  }
+}
