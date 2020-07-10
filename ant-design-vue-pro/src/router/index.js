6b9fa755c59edb5f639c69480e10af746d3a5520
Andy Chen <chenyangpoppin@hotmail.com>
2020-07-10 16:03:26 +0800

    Add /login and register router configuration

--- ant-design-vue-pro/src/router/index.js
+++ ant-design-vue-pro/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import RenderRootView from "@/components/RenderRootView.vue";
 
 Vue.use(VueRouter);
 
@@ -16,8 +17,24 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/user",
+    name: "user",
+    component: RenderRootView,
+    children: [
+      {
+        path: "/user/login",
+        name: "login",
+        component: () => import(/* webpackChunkName: "user" */ "../views/User/Login.vue")
+      },
+      {
+        path: "/user/register",
+        name: "register",
+        component: () => import(/* webpackChunkName: "user" */ "../views/User/Register.vue")
+      }
+    ]
   }
 ];
 
