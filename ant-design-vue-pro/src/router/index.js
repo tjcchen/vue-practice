d455867ca7ec9e03dae5df6378294bed577072c7
Andy Chen <chenyangpoppin@hotmail.com>
2020-07-13 11:50:04 +0800

    Utilize NProgress plugin when loading pages

--- ant-design-vue-pro/src/router/index.js
+++ ant-design-vue-pro/src/router/index.js
@@ -4,6 +4,10 @@ import VueRouter from "vue-router";
 // import RenderRootView from "@/components/RenderRootView.vue";
 import NotFound from "../views/404.vue";
 
+// NPM NProgress plugin
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -84,6 +88,15 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export default router;
