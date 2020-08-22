3365d324d6eac9a7ce5dcaa68f3e06b081041968
Andy Chen <chenyangpoppin@hotmail.com>
2020-08-22 16:31:13 +0800

    Try different approaches to load svg images

--- ant-design-vue-pro/src/views/404.vue
+++ ant-design-vue-pro/src/views/404.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="wrapper-404">
+    <!-- solution1: import icon font logo -->
     <IconFont class="icon-404" type="icon-404"></IconFont>
+
+    <!-- Note: solution 2 and solution 3 cannot coexist due webpack configuration clear all existing loader -->
+    <!-- Therefore, I comment out its usage -->
+    <!-- solution2: import svg logo from local resources -->
+    <!-- img :src="NotFoundImage" alt="NotFountImage" / -->
+
+    <!-- solution3: use vue cli svg loader  -->
+    <Google />
   </div>
 </template>
 
 <script>
-export default {};
+import NotFoundImage from "@/assets/NotFound.svg";
+import Google from "@/assets/google.svg";
+
+export default {
+  data() {
+    return {
+      NotFoundImage
+    };
+  },
+  components: {
+    Google
+  }
+};
 </script>
 
 <style scoped lang="less">
--- ant-design-vue-pro/vue.config.js
+++ ant-design-vue-pro/vue.config.js
@@ -9,6 +9,20 @@ module.exports = {
       }
     }
   },
+
+  // Add SVG loader
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+
+    // clear all existing loaders.
+    // if you don't do this, the loader below will be appended to
+    // existing loaders of the rule.
+    svgRule.uses.clear()
+
+    // add replacement loader(s)
+    svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+  },
+
   // https://webpack.docschina.org/configuration/dev-server/#devserverproxy
   devServer: {
     proxy: {
@@ -22,7 +36,7 @@ module.exports = {
             console.log('Skipping proxy for browser request 222.');
 
             return '/index.html';
-          } else if (process.env.MOCK !== 'none') {  // apply this part of logic when environment is mock
+          } else if (process.env.MOCK !== 'none' && req.path.indexOf('api') !== -1) {  // apply this part of logic when environment is mock
             const name   = req.path.split("/api/")[1].split("/").join("_");
             const mock   = require(`./mock/${name}`);
             const result = mock(req.method);
