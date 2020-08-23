e81ea18f82de35f7039d044474a8f1c64ab694c3
Andy Chen <chenyangpoppin@hotmail.com>
2020-08-23 18:01:52 +0800

    Change some commentations to elaborate svg load issues

--- ant-design-vue-pro/src/views/404.vue
+++ ant-design-vue-pro/src/views/404.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="wrapper-404">
-    <!-- solution1: import icon font logo -->
+    <!-- Three solutions to load svg images to the website -->
+    <!-- Solution1: import icon font logo -->
     <IconFont class="icon-404" type="icon-404"></IconFont>
 
-    <!-- Note: solution 2 and solution 3 cannot coexist due webpack configuration clear all existing loader -->
-    <!-- Therefore, I comment out its usage -->
-    <!-- solution2: import svg logo from local resources -->
+    <!-- Note: solution 2 and solution 3 cannot coexist due to webpack configuration clear all existing loaders -->
+    <!-- Therefore, I commented out its usage -->
+    <!-- Solution2: import svg logo from local resources -->
     <!-- img :src="NotFoundImage" alt="NotFountImage" / -->
 
-    <!-- solution3: use vue cli svg loader  -->
+    <!-- Solution3: use vue cli svg loader  -->
     <Google />
   </div>
 </template>
