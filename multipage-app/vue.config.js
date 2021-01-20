bba0bac38f8396fd524f89f43c03c57ea8d87d1c
Andy Chen <chenyangpoppin@hotmail.com>
2021-01-20 11:33:36 +0800

    feat: add simple webpack config

--- multipage-app/vue.config.js
+++ multipage-app/vue.config.js
@@ -1,4 +1,8 @@
+const HTMLInlineCssWebpackPlugin  = require('html-inline-css-webpack-plugin').default;
+const HardSourceWebpackPlugin     = require('hard-source-webpack-plugin');
+
 module.exports = {
+  // multipage application entry configuration
   pages: {
     'index': {
       entry: './src/pages/home/main.js',
@@ -27,5 +31,25 @@ module.exports = {
         additionalData: `@import '@/style/common.less';`
       }
     }
+  },
+
+  // working with webpack( simple config )
+  configureWebpack: {
+    plugins: [
+      new HTMLInlineCssWebpackPlugin(), // put css on page directly
+      new HardSourceWebpackPlugin() // use cache for build
+    ]
   }
+
+  // working with webpack( choose different configs for production and development environments )
+  // configureWebpack: config => {
+  //   console.log(`---${process.env.NODE_ENV}---`);
+  //   console.log(`---${process.env.APP_NAME}---`); // configured in .env file
+  //   console.log(`---${process.env.AUTHOR}---`);
+  //   if (process.env.NODE_ENV === 'production') {
+  //     // mutate config for production
+  //   } else {
+  //     // mutate config for development
+  //   }
+  // }
 };
