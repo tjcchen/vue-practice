f78eb42364d1b7d87cf40e69f10af2eb5165f748
Andy Chen <chenyangpoppin@hotmail.com>
2021-01-25 22:35:47 +0800

    feat: add blogs filter with vue mixin

---
+++ vue-mixin/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <show-blogs></show-blogs>
+    <hr/>
+    <list-blogs></list-blogs>
+  </div>
+</template>
+
+<script>
+import showBlogs from './components/showBlogs.vue';
+import listBlogs from './components/listBlogs.vue';
+
+export default {
+  name: 'App',
+  components: {
+    'show-blogs': showBlogs,
+    'list-blogs': listBlogs
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-mixin/src/components/showBlogs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="show-blogs">
+    <h2>All Blog Articles</h2>
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <div v-for="blog in filteredBlogs" v-bind:key="blog.id" class="single-blog">
+      <h2>{{ blog.title }}</h2>
+      <article>{{ blog.body }}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      blogs: [],
+      search: ''
+    };
+  },
+  methods: {
+
+  },
+  created() {
+    const vm = this;
+
+    axios.get('http://jsonplaceholder.typicode.com/posts').then(function(response) {
+      vm.$data.blogs = response.data.slice(0, 5);
+    });
+  },
+  computed: {
+    filteredBlogs: function() {
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
